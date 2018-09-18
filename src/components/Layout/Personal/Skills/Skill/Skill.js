3733b519f731b9b02e1e7b6ee81523f3459534e1
User Peelar <peelar@s24.mydevil.net>
2018-09-18 16:06:31 +0200

    Fix

--- src/components/Layout/Personal/Skills/Skill/Skill.js
+++ src/components/Layout/Personal/Skills/Skill/Skill.js
@@ -5,7 +5,7 @@ import classes from './Skill.css';
 const skill = (props) => {
 	return (
 		<div className = {classes.skill} onMouseEnter={props.onHover} onMouseLeave={props.onLeave} name = {props.label}>
-			<p>{props.label}</p>
+			<p className = {classes.text}>{props.label}</p>
 			<p id = {props.label} className = {classes.advancement}>{props.advancementLevel}</p>
 		</div>
 	)
