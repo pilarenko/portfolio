88d4b10d7a625dfb824fbd72fc207e8c6b475d91
User Peelar <peelar@s24.mydevil.net>
2018-10-02 22:36:47 +0200

    Skills upgrade

--- src/components/Layout/Personal/Skills/Skill/Skill.js
+++ src/components/Layout/Personal/Skills/Skill/Skill.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 import classes from './Skill.css';
 
+
 const skill = (props) => {
+	const skillColor = {
+		"backgroundColor": props.skillColor,
+	};
+
 	return (
-		<div className = {classes.skill} onMouseEnter={props.onHover} onMouseLeave={props.onLeave} name = {props.label}>
-			<p className = {classes.text}>{props.label}</p>
-			<p id = {props.label} className = {classes.advancement}>{props.advancementLevel}</p>
+		<div>
+			<div className = {classes.skill} name = {props.label} style={skillColor}>
+				<span className={classes.gears}><i className="fas fa-cogs" /></span>
+			</div>
+			<p className = {classes.heading}>{props.label}</p>
+			<p className = {classes.text}>{props.text}</p>
 		</div>
+
 	)
 };
 export default skill;
--- src/components/Layout/Personal/Skills/Skills.js
+++ src/components/Layout/Personal/Skills/Skills.js
@@ -5,13 +5,13 @@ import Skill from './Skill/Skill';
 import classes from './Skills.css';
 
 const skills = (props) => {
-	const skillsList = ["ES6+", "React", "jQuery", "PHP", "HTML + CSS", "Linux", "Bootstrap", "Git", "Python"];
-	const advancementLevel = ["Advanced", "Advanced", "Pro", "Novice", "Advanced", "Novice", "Advanced", "Beginner", "Beginner"];
-
+	const skillsList = ["ES6 + jQuery", "React", "PHP", "HTML + CSS", "Linux", "Git"];
+	const skillLvl = ["3", "2", "1", "3", "1", "0"];
+	const skillColors = ["#f0be61", "#f0a461", "#f09061", "#f06f61"];
 	const elements = [];
 
-	for (let i = 0; i<skillsList.length; i++) {
-		elements.push(<Skill onHover={props.onHover} onLeave={props.onLeave} label = {skillsList[i]} key = {skillsList[i]} advancementLevel={advancementLevel[i]}/>);
+	for (let i = 0; i < skillsList.length; i++) {
+		elements.push(<Skill label = {skillsList[i]} key = {skillsList[i]} skillColor={skillColors[skillLvl[i]]} />);
 	}	
 
 	return (
